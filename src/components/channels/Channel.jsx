f13e65291d80f9137f92d302f2533f9a4c8f639d
moskowitza <moskowitzaaron@mac.com>
2019-12-24 07:58:13 -0500

    refactor Channel.jsx

--- src/components/channels/Channel.jsx
+++ src/components/channels/Channel.jsx
@@ -1,19 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-export default class Channel extends React.Component {
-  onClick () {
-    alert('bound click handler' + this.props.channel.name)
-  }
-  render () {
-    const { channel } = this.props
-    return (
-      <li onClick={this.onClick.bind(this)}>
-        {channel.name}
-      </li>
-    )
-  }
+export default function Channel ({ channel }) {
+  return (
+    <li>
+      {channel.name}
+    </li>
+  )
 }
+
 Channel.propTypes = {
   channel: PropTypes.object.isRequired
 }
