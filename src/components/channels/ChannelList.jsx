43671c2b6d4a831afb08dae17a99ff111edff7ba
moskowitza <moskowitzaaron@mac.com>
2019-12-24 08:00:07 -0500

    refactor ChannelList.jsx

--- src/components/channels/ChannelList.jsx
+++ src/components/channels/ChannelList.jsx
@@ -2,18 +2,15 @@ import React from 'react'
 import Channel from './Channel'
 import PropTypes from 'prop-types'
 
-export default class ChannelList extends React.Component {
-  render () {
-    const { channels } = this.props
-    return (
-      <ul>
-        {channels.map((channel, i) => {
-          console.log(channel)
-          return <Channel key={i} channel={channel} />
-        })}
-      </ul>
-    )
-  }
+export default function ChannelList({ channels }) {
+  return (
+    <ul>
+    {channels.map((channel, i) => {
+      console.log(channel)
+      return <Channel key={i} channel={channel} />
+    })}
+  </ul>
+  )
 }
 ChannelList.propTypes = {
   channels: PropTypes.array.isRequired,
